5da8c1491a24ea5e7b32cfb11d1f72847c5f532b
Bilal <bilal_norelden@hotmail.com>
2024-09-02 20:23:08 -0400

    some more tests

--- src/app.test.js
+++ src/app.test.js
@@ -10,3 +10,13 @@ test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/12/2020') );
     expect( day ).toBe('Thursday');
 });
+
+test('getDay returns the long-format day of the week', () => {
+  const day = dayOfTheWeek(new Date('3/13/2020'));
+  expect(day).toBe('Friday');
+});
+
+test('getDay returns the long-format day of the week', () => {
+  const day = dayOfTheWeek(new Date('3/14/2020'));
+  expect(day).toBe('Saturday');
+});
